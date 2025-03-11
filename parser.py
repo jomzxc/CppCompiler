9d4880dc7318ac9c8055ae5e1bd813e3c0dc78b0
jomzxc <202210892@fit.edu.ph>
2025-03-11 21:08:39 +0800

    ♻️ refactor(`parser.py`): Update docstrings for grammar rules to use consistent triple quotes.

--- parser.py
+++ parser.py
@@ -9,15 +9,17 @@ precedence = (
     ('left', 'TIMES', 'DIVIDE'),
 )
 
+
 def p_program(p):
-    '''program : function_list
-               | empty'''
+    """program : function_list
+               | empty"""
     p[0] = ('program', p[1])
 
+
 def p_function_list(p):
-    '''function_list : function
+    """function_list : function
                      | function_list function
-                     | empty'''
+                     | empty"""
     if len(p) == 2:
         if p[1] is None:
             p[0] = []
@@ -28,18 +30,20 @@ def p_function_list(p):
     else:
         p[0] = []
 
+
 def p_function(p):
-    '''function : TYPE ID LPAREN param_list RPAREN block
-                | TYPE ID LPAREN RPAREN block'''
+    """function : TYPE ID LPAREN param_list RPAREN block
+                | TYPE ID LPAREN RPAREN block"""
     if len(p) == 7:
         p[0] = ('function', p[1], p[2], p[4], p[6])
     else:
         p[0] = ('function', p[1], p[2], [], p[5])
 
+
 def p_param_list(p):
-    '''param_list : param
+    """param_list : param
                   | param_list COMMA param
-                  | empty'''
+                  | empty"""
     if len(p) == 2:
         if p[1] is None:
             p[0] = []
@@ -50,18 +54,21 @@ def p_param_list(p):
     else:
         p[0] = []
 
+
 def p_param(p):
-    '''param : TYPE ID'''
+    """param : TYPE ID"""
     p[0] = ('param', p[1], p[2])
 
+
 def p_block(p):
-    '''block : LBRACE statement_list RBRACE'''
+    """block : LBRACE statement_list RBRACE"""
     p[0] = ('block', p[2])
 
+
 def p_statement_list(p):
-    '''statement_list : statement
+    """statement_list : statement
                       | statement_list statement
-                      | empty'''
+                      | empty"""
     if len(p) == 2:
         if p[1] is None:
             p[0] = []
@@ -72,58 +79,66 @@ def p_statement_list(p):
     else:
         p[0] = []
 
+
 def p_statement(p):
-    '''statement : loop_statement
+    """statement : loop_statement
                  | declaration_statement
                  | expression SEMI
                  | return_statement SEMI
                  | block
-                 | declaration_statement_no_semi SEMI'''
+                 | declaration_statement_no_semi SEMI"""
     p[0] = p[1]
 
+
 def p_loop_statement(p):
-    '''loop_statement : FOR LPAREN for_init SEMI expression_opt SEMI expression_opt RPAREN block
-                      | WHILE LPAREN expression RPAREN block'''
+    """loop_statement : FOR LPAREN for_init SEMI expression_opt SEMI expression_opt RPAREN block
+                      | WHILE LPAREN expression RPAREN block"""
     if p[1] == 'for':
         p[0] = ('for_loop', p[3], p[5], p[7], p[9])
     elif p[1] == 'while':
         p[0] = ('while_loop', p[3], p[5])
 
+
 def p_for_init(p):
-    '''for_init : declaration_statement_no_semi
+    """for_init : declaration_statement_no_semi
                 | expression
-                | empty'''
+                | empty"""
     if len(p) > 1 and p[1] is not None:
         p[0] = p[1]
     else:
         p[0] = None
 
+
 def p_declaration_statement_no_semi(p):
-    '''declaration_statement_no_semi : TYPE ID
-                                     | TYPE ID ASSIGN expression'''
+    """declaration_statement_no_semi : TYPE ID
+                                     | TYPE ID ASSIGN expression"""
     if len(p) == 3:
         p[0] = ('declare', p[1], p[2], None)
     else:
         p[0] = ('declare', p[1], p[2], p[4])
 
+
 def p_declaration_statement(p):
-    '''declaration_statement : declaration_statement_no_semi'''
+    """declaration_statement : declaration_statement_no_semi"""
     p[0] = p[1]
 
+
 def p_return_statement(p):
-    '''return_statement : RETURN expression_opt'''
+    """return_statement : RETURN expression_opt"""
     p[0] = ('return', p[2])
 
+
 def p_expression_opt(p):
-    '''expression_opt : expression
-                      | empty'''
+    """expression_opt : expression
+                      | empty"""
     if len(p) > 1 and p[1] is not None:
         p[0] = p[1]
     else:
         p[0] = None
 
+
 def p_expression_binop(p):
-    '''expression : expression PLUS expression
+    """expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
                   | expression DIVIDE expression
@@ -132,34 +147,40 @@ def p_expression_binop(p):
                   | expression LT expression
                   | expression GT expression
                   | expression LEQ expression
-                  | expression GEQ expression'''
+                  | expression GEQ expression"""
     if len(p) == 4:
         p[0] = ('binop', p[2], p[1], p[3])
 
+
 def p_expression_assign(p):
-    '''expression : ID ASSIGN expression'''
+    """expression : ID ASSIGN expression"""
     p[0] = ('assign', p[1], p[3])
 
+
 def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
+    """expression : LPAREN expression RPAREN"""
     p[0] = p[2]
 
+
 def p_expression_call(p):
-    'expression : ID LPAREN arg_list RPAREN'
+    """expression : ID LPAREN arg_list RPAREN"""
     p[0] = ('call', p[1], p[3])
 
+
 def p_expression_id(p):
-    'expression : ID'
+    """expression : ID"""
     p[0] = ('id', p[1])
 
+
 def p_expression_number(p):
-    'expression : NUMBER'
+    """expression : NUMBER"""
     p[0] = ('number', p[1])
 
+
 def p_arg_list(p):
-    '''arg_list : expression
+    """arg_list : expression
                 | arg_list COMMA expression
-                | empty'''
+                | empty"""
     if len(p) == 2:
         if p[1] is None:
             p[0] = []
@@ -170,15 +191,18 @@ def p_arg_list(p):
     else:
         p[0] = []
 
+
 def p_empty(p):
-    'empty :'
+    """empty :"""
     p[0] = None
+
 
 def p_error(p):
     if p:
         print(f"Syntax error at line {p.lineno}, column {p.lexpos}, token {p.type} ({p.value})")
     else:
         print("Syntax error at EOF")
+
 
 parser = yacc.yacc()
 
@@ -188,8 +212,8 @@ parser = yacc.yacc()
 #     int main() {
 #             int x = 5;
 #             x = x + 3;
-#             while (x < 10) {
-#                 x = x * 2;
+#             for (int i=0; i<10; i=i+1) {
+#                 x = x * i;
 #             }
 #             return x;
 #         }
